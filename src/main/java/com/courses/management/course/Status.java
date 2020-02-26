7b22c082f7eeb146f3d387a12625b9c4e779b927
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2020-02-26 15:39:33 +0200

    Updated UserRole and Status enums.

--- src/main/java/com/courses/management/course/Status.java
+++ src/main/java/com/courses/management/course/Status.java
@@ -1,4 +1,17 @@
 package com.courses.management.course;
 
 public enum Status {
+    NOT_STARTED("NOT_STARTED"),
+    IN_PROGRESS("IN_PROGRESS"),
+    FINISHED("FINISHED");
+
+    private String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
--- src/main/java/com/courses/management/user/UserRole.java
+++ src/main/java/com/courses/management/user/UserRole.java
@@ -1,4 +1,17 @@
 package com.courses.management.user;
 
 public enum UserRole {
+    ADMIN("ADMIN"),
+    STUDENT("STUDENT"),
+    NEWCOMER("NEWCOMER");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
