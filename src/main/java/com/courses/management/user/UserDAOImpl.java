d5969400db86785495aeffb6c3f186a5b854e4d7
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2020-03-05 22:28:52 +0200

    Added UserDAO implementation.

---
+++ src/main/java/com/courses/management/user/UserDAOImpl.java
@@ -0,0 +1,32 @@
+package com.courses.management.user;
+
+import com.courses.management.common.DataAccessObject;
+
+import java.util.List;
+
+public class UserDAOImpl implements DataAccessObject<User> {
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public User get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
