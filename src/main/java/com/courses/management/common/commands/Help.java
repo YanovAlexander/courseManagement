ffb7fdf68daa82aac5c2de38186c814ebb20b6f2
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2020-03-14 13:25:42 +0200

    Added commands to help, update status, delete course, update title.

--- src/main/java/com/courses/management/common/commands/Help.java
+++ src/main/java/com/courses/management/common/commands/Help.java
@@ -3,6 +3,10 @@ package com.courses.management.common.commands;
 import com.courses.management.common.Command;
 import com.courses.management.common.View;
 import com.courses.management.common.commands.util.InputString;
+import com.courses.management.course.CourseStatus;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Help implements Command {
     private View view;
@@ -23,8 +27,35 @@ public class Help implements Command {
         view.write("\t| create_course|title");
         view.write("\t|\t-> create a course with a title");
         view.write("---------------------------------------------------------");
+
+        view.write("\t| find_course|title");
+        view.write("\t|\t-> find course by title");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| update_course_title|oldTitle|newTitle");
+        view.write("\t|\t-> update course title. Title is a unique field");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| update_course_status|title|status");
+        view.write("\t|\t-> update course status. Possible status values: ");
+        view.write("\t|\t-> " + collectCourseStatuses());
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| show_courses");
+        view.write("\t|\t-> show all courses");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| delete_course|title");
+        view.write("\t|\t-> move course to a DELETED status");
+        view.write("---------------------------------------------------------");
+
         view.write("\t| exit");
         view.write("\t|\t-> exit application");
         view.write("---------------------------------------------------------");
+        view.write("---------------------------------------------------------");
+    }
+
+    private String collectCourseStatuses() {
+        return Arrays.stream(CourseStatus.values()).map(CourseStatus::getStatus).collect(Collectors.joining(", "));
     }
 }
