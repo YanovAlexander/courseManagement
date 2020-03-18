e02f57014121483fdab49a5f2aa224056bcda9c2
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2020-03-18 19:58:31 +0200

    Added fnd user be email command

--- src/main/java/com/courses/management/common/commands/util/Commands.java
+++ src/main/java/com/courses/management/common/commands/util/Commands.java
@@ -9,6 +9,8 @@ public class Commands {
     public static final String SHOW_COURSES = "show_courses";
     public static final String UPDATE_COURSE_STATUS = "update_course_status|title|status";
     public static final String UPDATE_COURSE_TITLE = "update_course_title|oldTitle|newTitle";
+    public static final String CREATE_USER = "create_user|firstName|lastName|email";
+    public static final String FIND_USER = "find_user|email";
 
     private Commands() {
         throw new UnsupportedOperationException("Impossible to instantiate util class");
